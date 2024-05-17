defde9e72119ff91b991c6ffa939c6e26b2133f8
Renzo Jared <renzojared_lm@hotmail.com>
2024-05-17 16:03:43 -0500

    remove quotes

--- src/Domain/ValueObjects/Endpoints.cs
+++ src/Domain/ValueObjects/Endpoints.cs
@@ -4,20 +4,20 @@ public static class Endpoints
 {
     private const string api = nameof(api);
 
-    #region "Entities"
+    #region Entities
     private const string dossier = nameof(dossier);
     private const string overallprocess = nameof(overallprocess);
     private const string court = nameof(court);
     private const string person = nameof(person);
     #endregion
 
-    #region "Actions"
+    #region Actions
     private const string create = nameof(create);
     private const string getall = nameof(getall);
     private const string report = nameof(report);
     #endregion
 
-    #region "Endpoints"
+    #region Endpoints
     public const string CreateDossier = $"{api}/{dossier}/{create}";
     public const string GetAllOverallProcess = $"{api}/{overallprocess}/{getall}";
     public const string GetAllCourts = $"{api}/{court}/{getall}";
